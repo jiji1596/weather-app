cfdeb05dd2f751c795931886949eb5a89a96f446
Shunji Sugaya <sugayasc33@gmail.com>
2025-06-17 01:16:33 +0900

    fetching data from API done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function onChangeQuery(event) {
+    setSearchQuery(event.currentTarget.value);
+  }
+
+  function getData() {
+    const apiKey = "16459259815a0e8e9dc07e9595619fb3";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${apiKey}`
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+  }
 
   return (
     <>
     <div className='search-container'>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <input type="text"
+        value={searchQuery}
+        placeholder='Search a place'
+        className='input input-md'
+        onChange={onChangeQuery}
+        />
+      <button className="btn btn-primary" onClick={getData}>Large</button>
     </div>
     </>
   )
