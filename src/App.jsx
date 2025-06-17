dfa182fe12f8bbf42627f84ad862cb64de70b184
Shunji Sugaya <sugayasc33@gmail.com>
2025-06-17 14:46:22 +0900

    layout finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [data, setData] = useState(null);
 
   function WeatherCard({ weather }) {
-  return (
-    <>
+    return (
+      <>
+        <h2 className="text-4xl">{weather.name}</h2>
+        <div className="flex w-half">
+          <div className="card rounded-box grid h-24 grow place-items-center">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+              className="w-24 h-24"
+            />
+          </div>
+          <div className="card rounded-box grid h-24 grow place-items-center">
+            <p className="text-4xl font-bold">
+              {Math.round(weather.main.temp)}°C
+            </p>
+          </div>
+        </div>
 
-    <div className="flex gap-5">
-      <img
-        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-        alt={weather.weather[0].description}
-        className="w-32 h-32"
-      />
-      <div className="flex-col justify-center">
-        <h2 className="text-2xl">{weather.name}</h2>
-        <p className="text-4xl font-bold">{Math.round(weather.main.temp)}°C</p>
-      </div>
-    </div>
-
-
-    <span className="capitalize">{weather.weather[0].description}</span>
-    <div className="text-md mt-2 opacity-70">
-      Humidity: {weather.main.humidity}% • Wind: {weather.wind.speed} m/s
-    </div>
-    </>
-  );
-}
+        <span className="capitalize text-2xl">{weather.weather[0].description}</span>
+        <div className="text-lg mt-2 opacity-70">
+          Humidity: {weather.main.humidity}% • Wind: {weather.wind.speed} m/s
+        </div>
+      </>
+    );
+  }
 
   function onChangeQuery(event) {
     setSearchQuery(event.currentTarget.value);
@@ -36,10 +38,10 @@ function App() {
 
   function getData() {
     const apiKey = "16459259815a0e8e9dc07e9595619fb3";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${apiKey}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${apiKey}&units=metric`;
 
     fetch(url)
-      .then(resp => resp.json())
+      .then((resp) => resp.json())
       .then((data) => {
         setData(data);
       });
@@ -47,25 +49,28 @@ function App() {
 
   return (
     <>
-    <div className="search-container bg-base-100 shadow-xl mb-3">
-      <h1 className="font-mono text-secondary text-shadow-lg pt-3">Weather App</h1>
-      <div className="pt-3 flex gap-3 justify-center  ">
-        <input type="text"
-          value={searchQuery}
-          placeholder='Search a place'
-          className='input input-md'
-          onChange={onChangeQuery}
+      <div className="search-container bg-base-100 shadow-xl mb-3">
+        <h1 className="font-mono text-secondary text-shadow-lg pt-3">
+          Weather App
+        </h1>
+        <div className="pt-3 flex gap-3 justify-center  ">
+          <input
+            type="text"
+            value={searchQuery}
+            placeholder="Search a place"
+            className="input input-md"
+            onChange={onChangeQuery}
           />
-        <button className="btn btn-secondary" onClick={getData}>Search</button>
+          <button className="btn btn-secondary" onClick={getData}>
+            Search
+          </button>
+        </div>
+        <div className="card-body items-center">
+          {data && <WeatherCard weather={data} />}
+        </div>
       </div>
-      <div className="card-body items-center">
-        { data &&
-        < WeatherCard weather={data} />
-        }
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
